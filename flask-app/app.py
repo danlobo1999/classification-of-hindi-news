c27b0eb7dc4718f94ff2305390065c1a5e6dacab
Daniel Lobo <danlobo1999@gmail.com>
2021-05-07 19:12:46 +0530

    added: ajax function

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,5 +1,10 @@
 import os
+import sys
 from flask import Flask, request, render_template
+
+sys.path.append(os.path.abspath("../"))
+
+from hindibert.classify import Classify
 
 app = Flask(__name__, template_folder="templates")
 
@@ -8,15 +13,21 @@ app = Flask(__name__, template_folder="templates")
 def index():
     return render_template("index.html")
 
+
 # Upload API
-@app.route('/', methods=['POST'])
-def my_form_post():
-    output_text = request.form['text']
-    print(output_text)
+@app.route("/classify/", methods=["POST", "GET"])
+def classify():
+    if request.method == "POST":
+        hindi_bert = Classify()
+        input_text = [request.form.get("data")]
+
+        output_text = hindi_bert.predictor(text=input_text)
+        print(output_text)
+
+        return output_text
+
     return render_template("index.html", output_text=output_text)
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
